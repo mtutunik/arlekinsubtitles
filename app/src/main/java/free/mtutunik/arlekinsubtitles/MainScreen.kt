c538778bdf3730f3c8c4bb13233273e383a3db18
Misha Tyutyunik <Misha.Tyutyunik@mathworks.com>
2018-03-01 14:30:12 -0500

    Hide edit field when tapped outside

--- app/src/main/java/free/mtutunik/arlekinsubtitles/MainScreen.kt
+++ app/src/main/java/free/mtutunik/arlekinsubtitles/MainScreen.kt
@@ -3,22 +3,15 @@ package free.mtutunik.arlekinsubtitles
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.EditorInfo
-import android.widget.ImageButton
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main_screen.*
-import android.content.Context.INPUT_METHOD_SERVICE
 import android.view.inputmethod.InputMethodManager
 import android.preference.PreferenceManager
-import android.content.SharedPreferences
+import android.os.PowerManager
 import android.webkit.WebViewClient
-
-
-
-
 
 
 /**
@@ -27,15 +20,15 @@ import android.webkit.WebViewClient
  */
 class MainScreen : AppCompatActivity() {
     //private  lateinit var settingsBtn : ImageButton;
-    private var subtitlesUrl = "";
-
+    private var mSubtitlesUrl = "";
+    private var mWakeLock: PowerManager.WakeLock? = null
     private lateinit var ctx: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ctx = this
         val settings = PreferenceManager.getDefaultSharedPreferences(this)
-        subtitlesUrl = settings.getString("subtitlesurl", "")
+        mSubtitlesUrl = settings.getString("subtitlesurl", "")
 
         setContentView(R.layout.activity_main_screen)
         subtitlesView.webViewClient = WebViewClient()
@@ -43,37 +36,62 @@ class MainScreen : AppCompatActivity() {
         sourceUrl.setOnEditorActionListener(object: TextView.OnEditorActionListener {
             override fun onEditorAction(editView: TextView?, actionId: Int, evt: KeyEvent?): Boolean {
                 if (actionId == EditorInfo.IME_ACTION_SEND) {
-                    subtitlesUrl = editView?.text.toString()
+                    mSubtitlesUrl = editView?.text.toString()
                     sourceUrl.visibility = View.GONE
+                    channelName.visibility = sourceUrl.visibility
                     settingsBtn.visibility = View.VISIBLE
                     val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                     imm.hideSoftInputFromWindow(sourceUrl.getWindowToken(), 0)
 
                     val settings = PreferenceManager.getDefaultSharedPreferences(ctx)
                     val editor = settings.edit()
-                    editor.putString("subtitlesurl", subtitlesUrl)
+                    editor.putString("subtitlesurl", mSubtitlesUrl)
                     editor.commit()
-                    subtitlesView.loadUrl(SUBTITLES_SERVER_URL + "/" + subtitlesUrl)
+                    subtitlesView.loadUrl(SUBTITLES_SERVER_URL + "/" + mSubtitlesUrl)
                 }
                 return true
             }
         })
+
+        sourceUrl.setOnFocusChangeListener(object: View.OnFocusChangeListener {
+            override fun onFocusChange(view: View?, isFocus: Boolean) {
+                if (!isFocus) {
+                    sourceUrl.onEditorAction(EditorInfo.IME_ACTION_SEND)
+                }
+            }
+        })
+
         sourceUrl.visibility = View.GONE
         settingsBtn.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 sourceUrl.visibility = View.VISIBLE
-                sourceUrl.setText(subtitlesUrl)
+                channelName.visibility = sourceUrl.visibility
+                sourceUrl.setText(mSubtitlesUrl)
                 settingsBtn.visibility = View.GONE
                 sourceUrl.requestFocus()
                 val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                 imm.showSoftInput(sourceUrl, InputMethodManager.SHOW_IMPLICIT)
             }
         })
+
+
+        val pm = this.getSystemService(Context.POWER_SERVICE) as PowerManager
+        mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,"SubtitlesViewPowerLock")
+        mWakeLock?.setReferenceCounted(false)
+        if (!mWakeLock?.isHeld!!) {
+            mWakeLock?.acquire()
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mWakeLock?.release()
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
-        subtitlesView.loadUrl(SUBTITLES_SERVER_URL + "/" + subtitlesUrl)
+        subtitlesView.loadUrl(SUBTITLES_SERVER_URL + "/" + mSubtitlesUrl)
     }
 
 
